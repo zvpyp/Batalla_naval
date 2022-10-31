134ad73f4a86c765dda027089b3dd88cb103300c
Máximo Fernández <maximo03f@gmail.com>
2022-10-31 20:34:42 -0300

    Añadido menu de opciones

---
+++ batalla_naval/main.pas
@@ -0,0 +1,9 @@
+Program BatallaNaval;
+
+uses crt;
+
+{ Programa principal }
+begin
+    // Limpiar pantalla al iniciar.
+    clrscr;
+end.
---
+++ batalla_naval/menu.pas
@@ -0,0 +1,80 @@
+unit Menu;
+
+uses crt;
+
+{-----------------------------------------------}
+
+interface
+
+function Menu(): string;
+procedure CambiarColor();
+
+{-----------------------------------------------}
+
+implementation
+
+// mostrar el menú de opciones. Retorna la opción elegida.
+function Menu(): string;
+begin
+    Menu := '0';
+
+    while (Menu < '1') OR (Menu > '3') do
+    begin
+        writeln('1 - Jugar');
+        writeln('2 - Cambiar Tema');
+        writeln('3 - Salir');
+
+        Menu := ReadKey();
+        clrscr;
+    end;
+end;
+
+// TODO: hacer un menu de opciones para cambiar el color.
+procedure CambiarColor();
+var
+opt : char;
+color_letra : byte;
+color_fondo : byte;
+begin
+    opt := '0';
+
+    while (opt < '1') OR (opt > '5') do
+    begin
+        writeln('1 - Predeterminado');
+        writeln('2 - Invertido');
+        writeln('3 - Suave');
+        writeln('4 - Rosa');
+        writeln('5 - Naturaleza');
+
+        opt := ReadKey();
+        clrscr;
+    end;
+
+    case opt of
+    '1': begin 
+            color_letra := 15;
+            color_fondo := 0;
+        end;
+    '2': begin 
+            color_letra := 0;
+            color_fondo := 15;
+        end;
+    '3': begin 
+            color_letra := 7;
+            color_fondo := 3;
+        end;
+    '4': begin 
+            color_letra := 15;
+            color_fondo := 12;
+        end;
+    '5': begin 
+            color_letra := 11;
+            color_fondo := 2;
+        end;
+    end;
+
+    TextColor(color_letra);
+    TextBackground(color_fondo);
+end;
+
+end.
